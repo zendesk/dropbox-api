6a85bdbdc1b968964bff62ae19e08dad911e5477
petems <p.morsou@gmail.com>
2013-09-16 11:52:31 +0100

    Update 503 to give specific error

--- spec/lib/dropbox-api/connection_spec.rb
+++ spec/lib/dropbox-api/connection_spec.rb
@@ -79,11 +79,11 @@ describe Dropbox::API::Connection do
     end
 
     it "raises a Dropbox::API::Error when the response is a 503" do
-      response = double :code => 503, :body => '{ "error": "rate limited" }'
+      response = double :code => 503, :body => '{ "error": "rate limited" }', :headers => '{ "Retry-After": "50" }'
       lambda do
-        @connection.request { response }
-      end.should raise_error(Dropbox::API::Error, '503 - Possible Rate Limiting: rate limited')
-    end
+       @connection.request { response }
+     end.should raise_error(Dropbox::API::Error, '503 - rate limited. Retry after: 50')
+   end
 
     it "raises a Dropbox::API::Error::StorageQuota when the response is a 507" do
       response = double :code => 507, :body => '{ "error": "quote limit" }'
