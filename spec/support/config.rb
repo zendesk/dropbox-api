1ee02ab2ef7f52e2a21632edcfa24f67093d0264
Peter Souter <p.souter@kainos.com>
2014-09-07 21:17:19 +0100

    Better approach to config loading

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -1,19 +1,17 @@
 require "yaml"
 
-unless ENV['RECORDING'].nil?
+if ENV['RECORDING'] == 'true'
   config = YAML.load_file "spec/connection.yml"
-  Dropbox::API::Config.app_key    = config['app_key']
-  Dropbox::API::Config.app_secret = config['app_secret']
-  Dropbox::Spec.token             = config['token']
-  Dropbox::Spec.secret            = config['secret']
-  Dropbox::API::Config.mode       = config['mode']
 else
-  Dropbox::API::Config.app_key    = '4h2z7bkqpoxnrk5'
-  Dropbox::API::Config.app_secret = 'jm01iazkteh0iox'
-  Dropbox::Spec.token             = 'uaq3brdzaspkyyel'
-  Dropbox::Spec.secret            = 'ttg5h86znfxxfgf'
-  Dropbox::API::Config.mode       = 'dropbox'
+  config = YAML.load_file "spec/connection.offline.yml"
 end
+
+Dropbox::API::Config.app_key    = config['app_key']
+Dropbox::API::Config.app_secret = config['app_secret']
+Dropbox::API::Config.mode       = config['mode']
+
+Dropbox::Spec.token  = config['token']
+Dropbox::Spec.secret = config['secret']
 
 Dropbox::Spec.namespace = Time.now.to_i
 Dropbox::Spec.instance  = Dropbox::API::Client.new(:token  => Dropbox::Spec.token,
