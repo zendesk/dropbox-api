48becfde8e5078a6a1bb9213af02ce8b882c6eea
Peter Souter <p.souter@kainos.com>
2014-09-07 20:14:30 +0100

    Change support config

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -1,13 +1,19 @@
 require "yaml"
 
-config = YAML.load_file "spec/connection.yml"
-
-Dropbox::API::Config.app_key    = config['app_key']
-Dropbox::API::Config.app_secret = config['app_secret']
-Dropbox::API::Config.mode       = config['mode']
-
-Dropbox::Spec.token  = config['token']
-Dropbox::Spec.secret = config['secret']
+unless ENV['RECORDING'].nil?
+  config = YAML.load_file "spec/connection.yml"
+  Dropbox::API::Config.app_key    = config['app_key']
+  Dropbox::API::Config.app_secret = config['app_secret']
+  Dropbox::Spec.token             = config['token']
+  Dropbox::Spec.secret            = config['secret']
+  Dropbox::API::Config.mode       = config['mode']
+else
+  Dropbox::API::Config.app_key    = '4h2z7bkqpoxnrk5'
+  Dropbox::API::Config.app_secret = 'jm01iazkteh0iox'
+  Dropbox::Spec.token             = 'uaq3brdzaspkyyel'
+  Dropbox::Spec.secret            = 'ttg5h86znfxxfgf'
+  Dropbox::API::Config.mode       = 'dropbox'
+end
 
 Dropbox::Spec.namespace = Time.now.to_i
 Dropbox::Spec.instance  = Dropbox::API::Client.new(:token  => Dropbox::Spec.token,
